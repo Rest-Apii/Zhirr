98397233a3cb1eaf4d67a6fa2b9129b07ecddf26
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-03-10 22:06:43 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
+router.get('/about', (req, res) => {
+    res.sendFile(__path + '/views/about.html')
+})
+
 router.get('/config', (req, res) => {
     config = {
         status: true,
